b45d24fcff00eeb1dff3990fd7e788b841fb4fb2
Victor Giraldo <victor.giraldomestrada@gmail.com>
2024-12-24 14:47:09 -0500

    Display books on webpage, allow users to toggle read status and remove them.

--- public/js/script.js
+++ public/js/script.js
@@ -5,8 +5,17 @@ const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
 const read = document.querySelector("#read");
+const libraryContainer = document.querySelector(".library");
 
-const library = [];
+const library = [
+    new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, true),
+    new Book('To Kill a Mockingbird', 'Harper Lee', 281, false),
+    new Book('1984', 'George Orwell', 328, true),
+    new Book('Pride and Prejudice', 'Jane Austen', 279, false),
+    new Book('The Catcher in the Rye', 'J.D. Salinger', 214, true),
+];
+
+library.forEach((book) => addBookToDom(book));
 
 function Book(title, author, pages, read) {
     if (typeof title !== 'string' || title === '') {
@@ -44,7 +53,70 @@ closeModal.addEventListener('click', (e) => {
     modal.close();
     if (title.value !== '' && author.value !== '' && pages.value !== '') {
         const book = new Book(title.value, author.value, parseInt(pages.value), read.checked);
-        library.push(book);
-        // Add book to the DOM.
+        addBookToLibrary(book);
+        resetForm();
     }
 });
+
+function addBookToLibrary(book) {
+    if (!(book instanceof Book)) {
+        throw new Error('Book must be an instance of Book');
+    }
+    library.push(book);
+    addBookToDom(book);
+}
+
+function resetForm() {
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.checked = false;
+}
+
+function addBookToDom(book) {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    const bookTitle = document.createElement('h2');
+    bookTitle.classList.add('book-title');
+    bookTitle.textContent = book.title;
+    const bookAuthor = document.createElement('h3');
+    bookAuthor.classList.add('book-author');
+    bookAuthor.textContent = `By: ${book.author}`;
+    const bookPages = document.createElement('p');
+    bookPages.classList.add('book-pages');
+    bookPages.textContent = `${book.pages} pages.`;
+    const bookRead = document.createElement('button');
+    bookRead.classList.add('book-read');
+    bookRead.textContent = book.read ? 'Read' : 'Not Read';
+    if (book.read) {
+        bookRead.classList.add('read');
+    } else {
+        bookRead.classList.add('not-read');
+    }
+    bookRead.addEventListener('click', (e) => {
+        book.read = !book.read;
+        bookRead.textContent = book.read ? 'Read' : 'Not Read';
+        if (book.read) {
+            bookRead.classList.remove('not-read');
+            bookRead.classList.add('read');
+        } else {
+            bookRead.classList.remove('read');
+            bookRead.classList.add('not-read');
+        }
+    });
+    const bookRemove = document.createElement('button');
+    bookRemove.textContent = 'Remove';
+    bookRemove.classList.add('book-remove');
+
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookPages);
+    bookDiv.appendChild(bookRead);
+    bookDiv.appendChild(bookRemove);
+    libraryContainer.appendChild(bookDiv);
+    bookDiv.dataset.index = library.indexOf(book);
+    bookRemove.addEventListener('click', (e) => {
+        library.splice(library.indexOf(book), 1);
+        libraryContainer.removeChild(bookDiv);
+    });
+}
